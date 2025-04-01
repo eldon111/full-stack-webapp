f1c0768cfdffe82caf04807d1d41ab45eebde640
Eldon Mathias <eldon@emathias.com>
2025-04-01 12:51:41 -0500

    refactor the BE server and add CORS config

--- backend/app.ts
+++ backend/app.ts
@@ -3,27 +3,66 @@
 import path from "node:path"
 import AutoLoad from "@fastify/autoload"
 import {FastifyInstance, FastifyPluginAsync} from "fastify"
+import cors from "@fastify/cors";
+import {fastifyTRPCPlugin, FastifyTRPCPluginOptions} from "@trpc/server/adapters/fastify";
+import {AppRouter, appRouter} from "./routes/router";
+import {createContext} from "./routes/context";
+import closeWithGrace from "close-with-grace";
 
-const startServer: FastifyPluginAsync = async (server: FastifyInstance) => {
+const appService: FastifyPluginAsync = async (server: FastifyInstance) => {
 
-    // Place here your custom code!
+  // delay is the number of milliseconds for the graceful close to finish
+  closeWithGrace({delay: Number(process.env.FASTIFY_CLOSE_GRACE_DELAY) || 500}, async function ({signal, err, manual}) {
+    if (err) {
+      server.log.error(err)
+    }
+    await server.close()
+  })
 
-    // Do not touch the following lines
+  server.register(cors, {
+    origin: (origin, cb) => {
+      server.log.info(origin)
+      if (!origin) return cb(new Error("Not allowed"), false)
+      const hostname = new URL(origin).hostname
+      if (hostname === "localhost") {
+        server.log.info('localhost')
+        //  Request from localhost will pass
+        cb(null, true)
+      } else {
+        //   Generate an error on other origins, disabling access
+        cb(new Error("Not allowed"), false)
+      }
+    },
+  })
 
-    // This loads all plugins defined in plugins
-    // those should be support plugins that are reused
-    // through your application
-    server.register(AutoLoad, {
-        dir: path.join(__dirname, 'plugins'),
-        options: Object.assign({})
-    })
+  server.register(fastifyTRPCPlugin, {
+    prefix: '/trpc',
+    trpcOptions: {
+      router: appRouter,
+      createContext,
+      onError({path, error}) {
+        // report to error monitoring
+        console.error(`Error in tRPC handler on path '${path}':`, error);
+      },
+    } satisfies FastifyTRPCPluginOptions<AppRouter>['trpcOptions'],
+  })
 
-    // This loads all plugins defined in routes
-    // define your routes in one of these
-    server.register(AutoLoad, {
-        dir: path.join(__dirname, 'routes'),
-        options: Object.assign({})
-    })
+  // Do not touch the following lines
+
+  // This loads all plugins defined in plugins
+  // those should be support plugins that are reused
+  // through your application
+  server.register(AutoLoad, {
+    dir: path.join(__dirname, 'plugins'),
+    options: Object.assign({})
+  })
+
+  // This loads all plugins defined in routes
+  // define your routes in one of these
+  server.register(AutoLoad, {
+    dir: path.join(__dirname, 'routes'),
+    options: Object.assign({})
+  })
 }
 
-export default startServer
+export default appService
--- backend/server.ts
+++ backend/server.ts
@@ -1,27 +1,21 @@
 import * as dotenv from 'dotenv';
 import Fastify from "fastify";
-import closeWithGrace from "close-with-grace";
 import appService from "./app.js";
 
 dotenv.config();
 
-const app = Fastify({
-    logger: true
+const server = Fastify({
+  logger: true
 })
-app.register(appService)
 
-// delay is the number of milliseconds for the graceful close to finish
-closeWithGrace({ delay: Number(process.env.FASTIFY_CLOSE_GRACE_DELAY) || 500 }, async function ({ signal, err, manual }) {
-    if (err) {
-        app.log.error(err)
-    }
-    await app.close()
-})
+server.register(appService);
 
 // Start listening.
-app.listen({ port: Number(process.env.PORT) || 3000, host: '0.0.0.0' }, (err) => {
-    if (err) {
-        app.log.error(err)
-        process.exit(1)
-    }
-})
+(async () => {
+  try {
+    await server.listen({port: Number(process.env.PORT) || 3000, host: '0.0.0.0'});
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+})();
