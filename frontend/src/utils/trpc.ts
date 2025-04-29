502c878ba42ba438dfc35e7bd26a0ff0b2720924
Eldon Mathias <eldon@emathias.com>
2025-04-29 14:14:04 -0500

    hopefully fix FE->BE dep resolution

--- frontend/src/utils/trpc.ts
+++ frontend/src/utils/trpc.ts
@@ -1,4 +1,4 @@
-import type { AppRouter } from 'backend/routes/router';
+import type { AppRouter } from '@backend/routes/router';
 import { createTRPCContext } from '@trpc/tanstack-react-query';
 
 export const { TRPCProvider, useTRPC, useTRPCClient } = createTRPCContext<AppRouter>();
