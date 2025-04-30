4d036fc04bb9e5b7c7aa83e5490a5217f89cfdae
Eldon Mathias <eldon@emathias.com>
2025-04-30 11:14:17 -0500

    try to fix FE build issues

--- frontend/src/utils/trpc.ts
+++ frontend/src/utils/trpc.ts
@@ -1,4 +1,4 @@
-import type { AppRouter } from '@backend/routes/router';
+import { AppRouter } from '@emathias-web-app/backend-routes';
 import { createTRPCContext } from '@trpc/tanstack-react-query';
 
 export const { TRPCProvider, useTRPC, useTRPCClient } = createTRPCContext<AppRouter>();
