92df82ee3735216f1652205a40b6f90e410ba0af
Eldon Mathias <eldon@emathias.com>
2025-05-07 22:39:17 -0500

    fix global bucket namespace issue

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -2,15 +2,15 @@ module "full_stack_webapp" {
   source = "../../"
 
   project_id               = var.project_id
-  project_number = var.project_number
+  project_number           = var.project_number
   region                   = var.region
   environment              = "prod"
-  backend_service_name     = var.backend_service_name
-  frontend_service_name    = var.frontend_service_name
-  image_processor_name     = var.image_processor_name
-  image_bucket_name        = var.image_bucket_name
-  pubsub_topic_name        = var.pubsub_topic_name
-  pubsub_subscription_name = var.pubsub_subscription_name
+  backend_service_name     = "${var.backend_service_name}-prod"
+  frontend_service_name    = "${var.frontend_service_name}-prod"
+  image_processor_name     = "${var.image_processor_name}-prod"
+  image_bucket_name        = "${var.image_bucket_name}-prod"
+  pubsub_topic_name        = "${var.pubsub_topic_name}-prod"
+  pubsub_subscription_name = "${var.pubsub_subscription_name}-prod"
   frontend_domain_name     = var.frontend_domain_name
   frontend_url             = "https://${var.frontend_domain_name}"
   manage_dns               = var.manage_dns
